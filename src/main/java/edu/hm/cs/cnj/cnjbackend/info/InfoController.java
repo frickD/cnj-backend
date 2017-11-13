523687aad57f5c670445406ade5b2e6c6c40f77e
Daniel <daniel.frick@maihiro.com>
2017-11-13 22:22:26 +0100

    Config Setup

--- src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
+++ src/main/java/edu/hm/cs/cnj/cnjbackend/info/InfoController.java
@@ -1,45 +1,20 @@
 package edu.hm.cs.cnj.cnjbackend.info;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class InfoController {
 	
-	private final static String APPLICATIONPROPERTIES = "static/application.properties";
-	private final static String MESSAGEPROPERTY = "message";
+
+	@Autowired
+	InfoProperties properties;
 	
 	@RequestMapping("/info")
 	public Info greeting() {
-		Properties prop = new Properties();
-		InputStream input = null;
-		String response = "";
-
-		try {
-			ClassLoader classLoader = Thread.currentThread().getContextClassLoader();
-			input = classLoader.getResourceAsStream(APPLICATIONPROPERTIES);
-			prop.load(input);
-			response = prop.getProperty(MESSAGEPROPERTY);
-
-		} catch (IOException ex) {
-			ex.printStackTrace();
-			response = "Cloud not read from " + APPLICATIONPROPERTIES;
-		} finally {
-			if (input != null) {
-				try {
-					input.close();
-				} catch (IOException e) {
-					response = "Property " + MESSAGEPROPERTY + "" + APPLICATIONPROPERTIES;
-					e.printStackTrace();
-				}
-			}
-		}
 		Info info = new Info();
-		info.setMessage(response);
+		info.setMessage(properties.getMessage());
 		return info;
 	}
 }
